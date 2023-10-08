44297e6d239db34174bfad2061ea68db221a6d12
Luisandro Nachtigal Bandeira <luisandro@hotmail.com>
2023-10-08 15:24:31 -0300

    Configurando admin e atualizando model fotografia

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+# Se não criar essa classe para exbir a lista das fotografias no banco então será utilizada o __str__ do model Fotografia
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda")
+    list_display_links = ("id", "nome", "legenda")
+    # tem que ter uma virgula no final porque é esperado uma tupla
+    search_fields = ("nome", "legenda", )
+
+admin.site.register(Fotografia, ListandoFotografias)
