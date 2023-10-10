a4dc89bfbd8672569433b052733aa7c41de9bb1b
Luisandro Nachtigal Bandeira <luisandro@hotmail.com>
2023-10-10 10:59:49 -0300

    Implementado busca por tags de categorias e inicio do app usuários

--- galeria/views.py
+++ galeria/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from galeria.models import Fotografia
-def index(request):
 
-    dados = {
-        1: {"nome" : "Nebulosa de Carina",
-            "legenda" : "webbtelescope.org | NASA | James Webb"},
-        2: {"nome" : "Galáxia NGC 1079",
-            "legenda" : "nasa.org | NASA | Hubble"}
-    }
+def index(request):
     # Coloca-se um menos na frente do nome do campo para fazer order by desc
     fotografias = Fotografia.objects.order_by("-data_cadastro").filter(publicar=True)
     return render(request,'galeria/index.html', {"cards": fotografias})
@@ -16,4 +10,18 @@ def imagem(request, foto_id):
     # fotografia = Fotografia.objects.filter(id=foto_id).values()
     fotografia = get_object_or_404(Fotografia,pk=foto_id)
     return render(request,'galeria/imagem.html', {"fotografia":fotografia})
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by("-data_cadastro").filter(publicar=True)
+
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+            
+    if "categoria" in request.GET:        
+        nome_a_buscar = request.GET['categoria']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(categoria__icontains=nome_a_buscar)
+    return render(request,'galeria/buscar.html', {"cards":fotografias})
 
